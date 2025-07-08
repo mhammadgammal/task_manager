0bdbfc3eb93ce2f2e075f28a99fe028398ee0a57
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-07-08 16:21:47 +0300

    Add CORS support and static file serving
    Enables cross-origin resource sharing and static file serving capabilities:
    - Implements CORS middleware with all-origin (*) access
    - Adds static file serving from 'public' directory
    - Updates root endpoint to serve index.html from public folder

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const Cors = require('cors');
 const tasksRoute = require('./routes/tasksRoute');
 // connect to db
 require('./config/db.config');
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies
-
+app.use(Cors('*'))
+app.use(express.static('public')); // Serve static files from the 'public' directory
 app.use(
     '/api/v1/tasks',
     tasksRoute
 )
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Task API');
+    res.send('public/index.html');
 });
 
 const PORT = 3000;
