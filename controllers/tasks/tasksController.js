71b7a91d880eb65fe15bf02f7bf301a8ca871478
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-07-08 16:18:41 +0300

    task route controller done with the essintial CRUD and basic validation

--- controllers/tasks/tasksController.js
+++ controllers/tasks/tasksController.js
@@ -1,21 +1,43 @@
 const Task = require('../../models/taskModel');
 
-const getAllTasks = (req, res) => {
-    res.status(200).json({
-        message: 'Get all tasks'
-    });
+const getAllTasks = async (_, res) => {
+    try {
+        const tasks = await Task.find();
+
+        res.status(200).json({ status: 'success', tasks });
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to fetch tasks",
+            error: error.message
+        });
+    }
 }
 
-const getTaskById = (req, res) => {
-    const { id } = req.params;
-    res.status(200).json({
-        message: `Get task with ID: ${id}`
-    });
+const getTaskById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findById(id)
+        if (!task) {
+            throw new Error(`No task found with ID: ${id}`);
+        }
+        res.status(200).json({
+            status: 'success',
+            task: task
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message,
+        });
+    }
 }
+
 const createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body);
-        res.status(201).json({ task });
+        res.status(201).json({
+            status: 'success',
+            task
+        });
     } catch (error) {
         res.status(400).json(
             {
@@ -26,16 +48,49 @@ const createTask = async (req, res) => {
     }
 }
 
-const updateTask = (req, res) => {
-    res.status(200).json({
-        message: 'Update a task'
-    });
+const updateTask = async(req, res) => {
+    try {
+        const { id } = req.params;
+        console.log(`Updating task with ID: ${id}`, req.body);
+        
+        const task = await Task.findByIdAndUpdate({ _id: id }, req.body,
+            {
+                new: true,
+                runValidators: true
+            })
+
+        if (!task) {
+            throw new Error(`No task found with ID: ${id}`);
+        }
+
+        res.status(200).json({
+            status: 'success',
+            task
+        });
+
+    } catch (error) {
+        res.status(404).json({
+            message: error.message,
+        });
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.status(200).json({
-        message: 'Delete a task'
-    })
+const deleteTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findByIdAndDelete(id);
+        if (!task) {
+            throw new Error(`No task found with ID: ${id}`);
+        }
+        res.status(200).json({
+            status: 'success',
+            message: 'Task deleted successfully'
+        });
+    } catch (err) {
+        res.status(404).json({
+            message: err.message,
+        });
+    }
 }
 
 module.exports = {
