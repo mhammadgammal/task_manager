86cf796e0b7222832c0ed40e23be1bcb53c7a773
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-07-08 12:43:02 +0300

    implement tasks route - one for "tasks" enpoint - any request with path parameter

---
+++ routes/tasksRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {
+    getAllTasks,
+    getTaskById,
+    createTask,
+    updateTask,
+    deleteTask
+} = require('../controllers/tasksController');
+const router = express.Router();
+
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTaskById).patch(updateTask).delete(deleteTask);
+
+module.exports = router;
